6b789d45019bef25ad4a487b91f7a65df517356b
Pooja-M-R <71132064+Pooja-M-R@users.noreply.github.com>
2020-12-29 14:16:54 +0530

    Initial commit

---
+++ Program-2.c
@@ -0,0 +1,15 @@
+//INPUT -3
+//OUTPUT - 1 2 3
+
+
+#include<stdio.h>
+void main()
+{
+	int i=1,n;
+	printf("Enter upper limit : ");
+	scanf("%d",&n);
+	printf("The natural numbers are ");
+	for(i=1;i<=n;i++)
+		printf("%d ",i);
+	//Fill the missing code here
+ }
---
+++ Program-4.c
@@ -0,0 +1,18 @@
+//INPUT- 1
+//OUTPUT-25
+
+#include <stdio.h>
+int main ()
+{
+ int a,sum = 0;
+//Fill the missing code
+ {
+   
+   if ( a % 2 == 0 )
+      continue;
+  sum = sum + a;
+  }
+/Fill the missing code
+ return 0;
+}
+
